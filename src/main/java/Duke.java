0e72f8a0a42702f43c15f1fb0661777655279220
sophiien <sophieng1366@gmail.com>
2022-01-20 00:22:57 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner s = new Scanner(System.in);
         int j = 0;
-        Task[] list = new Task[100];
+        ArrayList<Task> list = new ArrayList<>();
         String[] input = s.nextLine().split(" ");
         while (!(input[0].toLowerCase().equals("bye"))) {
             if (input[0].toLowerCase().equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
-                for (int i = 1; i < list.length && list[i - 1] != null; i++) {
-                    System.out.println(i + ". " + list[i - 1]);
+                for (int i = 1; i <= list.size() && list.get(i - 1) != null; i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
                 }
                 input = s.nextLine().split(" ");
                 continue;
             } else if (input[0].toLowerCase().equals("mark")) {
                 int i = Integer.parseInt(input[1]);
-                list[i - 1].mark();
-                System.out.println("Nice! I've marked this task as done:\n" + list[i - 1]);
+                list.get(i-1).mark();
+                System.out.println("Nice! I've marked this task as done:\n" + list.get(i - 1));
                 input = s.nextLine().split(" ");
                 continue;
             } else if (input[0].toLowerCase().equals("unmark")) {
                 int i = Integer.parseInt(input[1]);
-                list[i - 1].unmark();
-                System.out.println("OK, I've marked this task as not done yet:\n" + list[i - 1]);
+                list.get(i - 1).unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + list.get(i-1));
                 input = s.nextLine().split(" ");
                 continue;
             } else {
@@ -38,10 +39,10 @@ public class Duke {
                     for (int i = 1; i < input.length; i++) {
                         name += " " + input[i];
                     }
-                    list[j] = new Todo(name);
-                    System.out.println("Got it. I have added this task: " + list[j]);
+                    list.add(new Todo(name));
+                    System.out.println("Got it. I have added this task: " + list.get(j));
                     j += 1;
-                    System.out.println("Now you have " + j + " in the list");
+                    System.out.println("Now you have " + j + " items in the list");
                     input = s.nextLine().split(" ");
                     continue;
                 } else if (input[0].toLowerCase().equals("deadline") || input[0].toLowerCase().equals("event")) {
@@ -61,7 +62,7 @@ public class Duke {
                             }
                             name += " " + input[i];
                         }
-                        list[j] = new Deadline(name, deadline);
+                        list.add(new Deadline(name, deadline));
 
                     } else if (input[0].toLowerCase().equals("event")) {
                         if (input.length == 1) {
@@ -78,11 +79,19 @@ public class Duke {
                             }
                             name += " " + input[i];
                         }
-                        list[j] = new Event(name, deadline);
+                        list.add(new Event(name, deadline));
                     }
-                    System.out.println("Got it. I have added this task: " + list[j]);
+                    System.out.println("Got it. I have added this task: " + list.get(j));
                     j += 1;
-                    System.out.println("Now you have " + j + " in the list");
+                    System.out.println("Now you have " + j + " item in the list");
+                    input = s.nextLine().split(" ");
+                    continue;
+                } else if (input[0].equals("delete")) {
+                    int i = Integer.parseInt(input[1]);
+                    Task t = list.remove(i-1);
+                    j--;
+                    System.out.println("Noted. I've removed this task: \n" + t);
+                    System.out.println("Now you have " + j + " item in the list");
                     input = s.nextLine().split(" ");
                     continue;
                 } else {
