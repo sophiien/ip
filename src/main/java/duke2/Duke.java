1b66dc9805ead2559a6486a283983aaa6b9fa036
sophiien <sophieng1366@gmail.com>
2022-02-06 00:32:48 +0800

    Added javafx in build.gradle

--- src/main/java/duke2/Duke.java
+++ src/main/java/duke2/Duke.java
@@ -1,17 +1,8 @@
 package duke2;// package duke1;
 
-import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
-
-import com.sun.tools.javac.Main;
-import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class Duke  {
 
@@ -47,34 +38,6 @@ public class Duke  {
         ui.endMsg();
     }
 
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-//    private void handleUserInput() {
-//        Label userText = new Label(userInput.getText());
-//        Label dukeText = new Label(getResponse(userInput.getText()));
-//        dialogContainer.getChildren().addAll(
-//                DialogBox.getUserDialog(userText, new ImageView(user)),
-//                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-//        );
-//        userInput.clear();
-//    }
     public String getResponse(String input) {
         return "Repeat: " + input;
     }
